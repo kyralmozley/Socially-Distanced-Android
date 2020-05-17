868fb2fc130f2ce835ed68e336b458d297d63c28
Kyra <kyralmozley@outlook.com>
2020-05-17 04:30:13 +0100

    Can now get our API response for a searched place

--- app/src/main/java/space/sociallydistanced/MainActivity.java
+++ app/src/main/java/space/sociallydistanced/MainActivity.java
@@ -4,10 +4,14 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentActivity;
 
+import android.accounts.NetworkErrorException;
+import android.annotation.SuppressLint;
 import android.app.Fragment;
+import android.app.ProgressDialog;
 import android.graphics.Color;
 import android.location.Address;
 import android.location.Geocoder;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.security.keystore.UserNotAuthenticatedException;
 import android.util.Log;
@@ -30,9 +34,22 @@ import com.google.android.libraries.places.api.net.PlacesClient;
 import com.google.android.libraries.places.widget.AutocompleteSupportFragment;
 import com.google.android.libraries.places.widget.listener.PlaceSelectionListener;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.Arrays;
 import java.util.List;
+
+import javax.net.ssl.HttpsURLConnection;
 
 public class MainActivity extends FragmentActivity implements OnMapReadyCallback {
 
@@ -41,6 +58,9 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
     SupportMapFragment mapFragment;
     SearchView searchView;
     PlacesClient placesClient;
+
+    String placeID;
+    JSONObject results;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -66,9 +86,15 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
                 new PlaceSelectionListener() {
                     @Override
                     public void onPlaceSelected(@NonNull Place place) {
+                        placeID = place.getId();
+                        new GetJSONTask().execute(placeID);
+                        Log.d(TAG, "it worked!!!!! 4am");
+                        Log.d(TAG, String.valueOf(results));
+
                         final LatLng latLng = place.getLatLng();
                         map.addMarker(new MarkerOptions().position(latLng).title(place.getName()));
                         map.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 15));
+
                     }
 
                     @Override
@@ -84,5 +110,51 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         map = googleMap;
+        CameraUpdate center = CameraUpdateFactory.newLatLngZoom(new LatLng(53, -3), 5);
+        map.animateCamera(center);
     }
+
+    // Uses AsyncTask to create a task away from the main UI thread(For Avoid
+    // NetworkOnMainThreadException). This task takes a
+    // URL string and uses it to create an HttpUrlConnection. Once the
+    // connection
+    // has been established, the AsyncTask downloads the contents of the data as
+    // an InputStream. Than, the InputStream is converted into a string, which
+    // is
+    // displayed in the TextView by the AsyncTask's onPostExecute method. Which
+    // called after doInBackgroud Complete
+    private class GetJSONTask extends AsyncTask {
+        private ProgressDialog pd;
+
+
+        @Override
+        protected Object doInBackground(Object[] objects) {
+            try {
+                String data = Utility.getData((String) objects[0]);
+                onPostExecute(data);
+                Log.d(TAG, "data: " + data);
+                results = new JSONObject(data);
+                Log.d(TAG, "json: " + results);
+                return data;
+            } catch (IOException | JSONException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        // onPreExecute called before the doInBackgroud start for display
+        // progress dialog.
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            pd = ProgressDialog.show(MainActivity.this, "", "Loading", true,
+                    false); // Create and show Progress dialog
+        }
+
+        protected void onPostExecute(String result) throws JSONException {
+            pd.dismiss();
+        }
+
+    }
+
 }
---
+++ app/src/main/java/space/sociallydistanced/Utility.java
@@ -0,0 +1,52 @@
+package space.sociallydistanced;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.StringWriter;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.util.Log;
+
+import javax.net.ssl.HttpsURLConnection;
+
+public class Utility {
+    private static String TAG = "Utility Log";
+
+    public static String getData(String myurl) throws IOException {
+        InputStream is = null;
+
+        try {
+            URL url = new URL("https://ec2.sociallydistanced.space/api/main/place?placeId=" + myurl);
+            HttpsURLConnection connection = (HttpsURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+            connection.setDoInput(true);
+
+            connection.connect();
+            Log.d(TAG, "The response is " + connection.getResponseCode());
+            is = connection.getInputStream();
+
+            String contentAsString = readInputStream(is);
+            return contentAsString;
+        } finally {
+            if(is != null) {
+                is.close();
+            }
+        }
+    }
+
+    private static String readInputStream(InputStream is) throws IOException {
+        int n = 0;
+        char[] buffer = new char[1024 * 4];
+        InputStreamReader reader = new InputStreamReader(is, "UTF8");
+        StringWriter writer = new StringWriter();
+        while (-1 != (n = reader.read(buffer)))
+            writer.write(buffer, 0, n);
+        return writer.toString();
+    }
+}
